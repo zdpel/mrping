77eebcb5703ea87f34d3632a59109c87a65895c5
zdpel <zdpelham@gmail.com>
2024-04-03 17:07:36 -0500

    Tab view added with unique appbar for dashboard and settings page

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,98 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:mrping/screens/playerstatspage.dart';
-import '../db/mainDB.dart';
-import '../db/player.dart';
+import 'package:mrping/screens/gamehistory.dart';
+import 'package:mrping/screens/leaderboard.dart';
 
-class Dashboard extends StatefulWidget {
+class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
   @override
-  State<Dashboard> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<Dashboard> {
-  late List<Player> players = [];
-  int nextID = 1;
-  int myIndex = 0;
-
-  @override
-  void initState(){
-    super.initState();
-    refreshPlayers();
-  }
-
-  Future refreshPlayers() async {
-    players = await mainDB.instance.readAllPlayerInfo();
-    players.sort((a,b) => b.rating!.compareTo(a.rating as num));
-    setState(() {}); 
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-        child: Column(
-          children: <Widget>[
-            const ListTile(
-              leading: Text("Rank", style: TextStyle(fontSize: 15.0)),
-              title: Padding(
-                padding: EdgeInsets.only(left: 15.0),
-                child: Text(
-                  "Name",
-                  style: TextStyle(fontSize: 15.0),
-                )
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Dashboard"),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: "Leaderboard"
               ),
-              trailing: Text("Rating", style: TextStyle(fontSize: 15.0))
-            ),
-            if (players.isNotEmpty)
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: players.length,
-                  itemBuilder: (context, index){
-                    return GestureDetector(
-                      onTap: (){
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                              PlayerStatsPage(player: players[index]),
-                          ),
-                        );
-                      },
-                      child: Card(
-                        child: ListTile(
-                          leading: Padding(
-                            padding: const EdgeInsets.only(left: 8.0),
-                            child: Text(
-                              //ID ${players[index].id}
-                              '${index+1}',
-                              style: const TextStyle(fontSize: 20.0),
-                            ),
-                          ),
-                          title: Padding(
-                            padding: const EdgeInsets.only(left: 25.0),
-                            child: Text(
-                              '${players[index].name}',
-                              style: const TextStyle(fontSize: 20.0),
-                            )
-                          ),
-                          trailing: Text(
-                            '${players[index].rating}',
-                            style: const TextStyle(fontSize: 20.0),
-                          ),
-                        )
-                        
-                      ),
-                    );
-                  }
-                ),
-              ),
-            if (players.isEmpty)
-              Text(
-                "No db entry available",
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
+              Tab(
+                text: "Game History"
+              )
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Leaderboard(),
+            GameHistory()
           ],
         ),
       ),
